da2a7bcca790b37d1aded7ae3fb93ef41bb34bba
Mars Liu <mars.liu@outlook.com>
2020-07-17 03:00:38 +0800

    expression parser

--- src/main/java/jisp/Repl.java
+++ src/main/java/jisp/Repl.java
@@ -1,5 +1,10 @@
 package jisp;
 
+import jaskell.expression.Env;
+import jaskell.expression.Expression;
+import jaskell.expression.ExpressionException;
+import jaskell.expression.parser.Parser;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,11 +19,19 @@ import java.io.InputStreamReader;
 public class Repl {
     private static final String prmt = ">> ";
     public static void main(String[] args) throws IOException {
+        Parser parser = new Parser();
+        Env env = new Env();
         while (true) {
             System.out.print(prmt);
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String line = reader.readLine();
-            System.out.println(line);
+            Expression expression = parser.parse(line);
+            expression = expression.makeAst();
+            try {
+                System.out.println(expression.eval(env));
+            } catch (ExpressionException e) {
+                System.out.println(String.format("%s parse error: %s", line, e.getMessage()));
+            }
         }
     }
 }
