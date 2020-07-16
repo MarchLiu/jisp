be44b2b46939e66277fc20dad56cbf8b2e933555
Mars Liu <mars.liu@outlook.com>
2020-07-16 23:40:02 +0800

    Hello Repl

---
+++ src/main/java/jisp/Repl.java
@@ -0,0 +1,17 @@
+package jisp;
+
+import java.io.IOException;
+
+/**
+ * TODO
+ *
+ * @author mars
+ * @version 1.0.0
+ * @since 2020/07/16 22:44
+ */
+public class Repl {
+    private static final String prmt = ">> ";
+    public static void main(String[] args) throws IOException {
+        System.out.print(prmt);
+    }
+}
